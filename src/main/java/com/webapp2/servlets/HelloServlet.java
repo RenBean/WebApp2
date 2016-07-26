fc00069507ba94c94b14c1ac5da8ec555446ce32
Ari Dalton <ren_logic@hotmail.com>
2016-07-26 11:31:28 -0600

    Initial commit

---
+++ src/main/java/com/webapp2/servlets/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.webapp2.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Ari on 7/26/16.
+ */
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet Get</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
+
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ari
+  Date: 7/26/16
+  Time: 10:14 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+Hello JSP
+</body>
+</html>
